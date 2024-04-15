b67e51bcfd1b937a10bada0ade31cf98ad0f8fd6
Mahmoud Mohamed <mm8871918@gmail.com>
2024-04-15 06:13:08 +0200

    Spring actuator endpoints and devtools and spring security starter and the default login page

---
+++ springboot/01-springboot-start/spring_actuator/src/main/java/com/springboot/spring_actuator/SpringActuatorApplication.java
@@ -0,0 +1,27 @@
+package com.springboot.spring_actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringActuatorApplication.class, args);
+	}
+
+}
+
+/*
+* spring-boot-starter-actuator
+http://localhost:8080/
+http://localhost:8080/task
+http://localhost:8080/actuator/beans
+http://localhost:8080/actuator/mappings
+http://localhost:8080/actuator/info
+
+*  spring-boot-starter-security
+default:
+    username = user
+    password is in "output"
+*/
---
+++ springboot/01-springboot-start/spring_actuator/src/test/java/com/springboot/spring_actuator/SpringActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.spring_actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
