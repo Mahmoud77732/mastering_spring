540cb85da0dcbbe73c9bf51b954bcd04f82c1d87
Mahmoud Mohamed <mm8871918@gmail.com>
2024-10-28 10:35:26 +0300

    start with the first app

---
+++ springguru/section1/boot-intro/src/main/java/com/springguru/bootintro/BootIntroApplication.java
@@ -0,0 +1,13 @@
+package com.springguru.bootintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootIntroApplication.class, args);
+	}
+
+}
---
+++ springguru/section1/boot-intro/src/test/java/com/springguru/bootintro/BootIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springguru.bootintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
